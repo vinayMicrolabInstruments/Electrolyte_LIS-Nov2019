6bc4ceb69264a82b7b38a42708217ed7282bc490
Viany <vinay@abhisystems.com>
2019-11-16 14:51:41 +0530

    Add project files.

---
+++ ElectrolyteLis/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace ElectrolyteLis
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSelect = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonFolder = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.textBoxIpaddress = new System.Windows.Forms.TextBox();
+            this.linkLabel = new System.Windows.Forms.LinkLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonSelect
+            // 
+            this.buttonSelect.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSelect.Location = new System.Drawing.Point(372, 35);
+            this.buttonSelect.Name = "buttonSelect";
+            this.buttonSelect.Size = new System.Drawing.Size(98, 26);
+            this.buttonSelect.TabIndex = 1;
+            this.buttonSelect.Text = "Start Server";
+            this.buttonSelect.UseVisualStyleBackColor = true;
+            this.buttonSelect.Click += new System.EventHandler(this.buttonSelect_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Server IP Address:";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 239);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(519, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(42, 17);
+            this.toolStripStatusLabel1.Text = "Status:";
+            // 
+            // toolStripStatus
+            // 
+            this.toolStripStatus.Name = "toolStripStatus";
+            this.toolStripStatus.Size = new System.Drawing.Size(17, 17);
+            this.toolStripStatus.Text = "\"\"";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(227, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Select  the Folder to Store:";
+            // 
+            // buttonFolder
+            // 
+            this.buttonFolder.Location = new System.Drawing.Point(252, 80);
+            this.buttonFolder.Name = "buttonFolder";
+            this.buttonFolder.Size = new System.Drawing.Size(92, 23);
+            this.buttonFolder.TabIndex = 4;
+            this.buttonFolder.Text = "Select Folder";
+            this.buttonFolder.UseVisualStyleBackColor = true;
+            this.buttonFolder.Click += new System.EventHandler(this.buttonFolder_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(19, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(141, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Selected Folder:";
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(236, 197);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(75, 23);
+            this.buttonExit.TabIndex = 1;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // textBoxIpaddress
+            // 
+            this.textBoxIpaddress.Enabled = false;
+            this.textBoxIpaddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxIpaddress.Location = new System.Drawing.Point(170, 34);
+            this.textBoxIpaddress.Name = "textBoxIpaddress";
+            this.textBoxIpaddress.Size = new System.Drawing.Size(174, 26);
+            this.textBoxIpaddress.TabIndex = 6;
+            // 
+            // linkLabel
+            // 
+            this.linkLabel.AutoSize = true;
+            this.linkLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel.Location = new System.Drawing.Point(167, 131);
+            this.linkLabel.Name = "linkLabel";
+            this.linkLabel.Size = new System.Drawing.Size(27, 16);
+            this.linkLabel.TabIndex = 7;
+            this.linkLabel.TabStop = true;
+            this.linkLabel.Text = "C:\\";
+            this.linkLabel.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(519, 261);
+            this.Controls.Add(this.linkLabel);
+            this.Controls.Add(this.textBoxIpaddress);
+            this.Controls.Add(this.buttonFolder);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonSelect);
+            this.Name = "Form1";
+            this.Text = "Data Receive Server:";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonSelect;
+        private System.Windows.Forms.Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatus;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonFolder;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.TextBox textBoxIpaddress;
+        private System.Windows.Forms.LinkLabel linkLabel;
+    }
+}
+
---
+++ ElectrolyteLis/Form1.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Globalization;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Diagnostics;
+
+namespace ElectrolyteLis
+{
+    public partial class Form1 : Form
+    {
+        Thread server;
+        String[] months = { "ETC", "Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec" };
+        public volatile bool stopserver = false;
+        string ipadd = "";
+        string foldername = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    textBoxIpaddress.Text =  ip.ToString();
+                }
+            }
+            foldername = linkLabel.Text.ToString();
+        }
+
+        private void buttonSelect_Click(object sender, EventArgs e)
+        {
+            buttonSelect.Enabled = false;
+            ipadd = textBoxIpaddress.Text.ToString();
+            server = new Thread(serverthread);
+            server.Start();
+
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Socket sk = (Socket)e.Argument;
+            String path ="";
+            char[] charsToTrim = { '\0', '.', ' ' };
+            byte[] b = new byte[1024];
+            string clientIPAddress = sk.LocalEndPoint.ToString();
+            int k = sk.Receive(b);
+            DateTime localDate = DateTime.Now;
+            int year = localDate.Year;
+            int month = localDate.Month;
+            int dayofmonth = localDate.Day;
+            path += year.ToString() + "\\";
+            path += months[month] + "\\";
+            path += dayofmonth.ToString();
+            path = foldername + path;
+
+            ASCIIEncoding asen = new ASCIIEncoding();
+            string result = "";
+            result = System.Text.Encoding.UTF8.GetString(b);
+            
+            result = result.TrimEnd(charsToTrim);      
+            if (Directory.Exists(path))
+            {
+                
+                using (System.IO.StreamWriter file =
+                new System.IO.StreamWriter(path+"\\Patientdata.csv", true))
+                {
+                    file.WriteLine(result);
+                    file.Flush();
+                    file.Close();
+                }
+                backgroundWorker1.ReportProgress(1, "Data Received from: "+ clientIPAddress);
+            }
+            else
+            {
+                Directory.CreateDirectory(path);
+
+                using (System.IO.StreamWriter file =
+                new System.IO.StreamWriter(path + "\\Patientdata.csv", true))
+                {
+                    file.WriteLine(result);
+                    file.Flush();
+                    file.Close();
+                }
+                backgroundWorker1.ReportProgress(1, "Data Received from: " + clientIPAddress);
+
+            }
+            sk.Send(asen.GetBytes("ACK\r\n"));
+            sk.Close();
+
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            try
+            {
+                toolStripStatus.Text = e.UserState.ToString();
+
+            }
+            catch(Exception)
+            {
+
+            }
+            
+        }
+
+        private void serverthread()
+        {
+            try
+            {
+
+                IPAddress ipAd = IPAddress.Parse(ipadd);
+                TcpListener myList = new TcpListener(ipAd, 3000);
+                myList.Start();
+                //("Waiting for a connection.....");
+                stopserver = true;
+                while (stopserver)
+                {
+                    Invoke(new Action(() => { toolStripStatus.Text = "Server Waiting for Connection:..."; }));
+                    Socket s = myList.AcceptSocket();
+                    backgroundWorker1.RunWorkerAsync(s);
+                }
+                myList.Stop();
+            }
+            catch (Exception )
+            {
+                //Console.WriteLine("Error..... " + e.StackTrace);
+            }Thread.Sleep(2000);
+            Invoke(new Action(()=>{ this.Close(); }));
+            
+        }
+
+        private void buttonFolder_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                linkLabel.Text = folderBrowserDialog1.SelectedPath;
+                foldername = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            StopServerThread();
+            
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            String path = String.Empty;
+            DateTime localDate = DateTime.Now;
+            int year = localDate.Year;
+            int month = localDate.Month;
+            int dayofmonth = localDate.Day;
+            path += year.ToString() + "\\";
+            path += months[month] + "\\";
+            path += dayofmonth.ToString();
+
+            path = foldername + path;
+            if (Directory.Exists(path))
+            {
+                Process.Start(path);
+                try
+                {
+                    //+ "\\Patientdata.csv"
+                    Process.Start(new ProcessStartInfo()
+                    {
+                        FileName = path + "\\Patientdata.csv",
+                        UseShellExecute = true,
+                        Verb = "open"
+                    });
+                }
+                catch (Exception we)
+                {
+                    MessageBox.Show(we.Message.ToString());
+                }
+
+            }
+
+
+        }
+
+        public  void StopServerThread()
+        {
+            if (!stopserver)
+            {
+                this.Close();
+                return;
+            }
+            stopserver = false;
+            // Data buffer for incoming data.  
+            byte[] bytes = new byte[1024];
+
+            // Connect to a remote device.  
+            try
+            {
+   
+                IPAddress ipAd = IPAddress.Parse(ipadd);
+                IPEndPoint remoteEP = new IPEndPoint(ipAd, 3000);
+                // Create a TCP/IP  socket.  
+                Socket sender = new Socket(ipAd.AddressFamily,
+                    SocketType.Stream, ProtocolType.Tcp);
+
+                // Connect the socket to the remote endpoint. Catch any errors.  
+                try
+                {
+                    sender.Connect(remoteEP);
+
+                    //Console.WriteLine("Socket connected to {0}",
+                    //    sender.RemoteEndPoint.ToString());
+
+                    // Encode the data string into a byte array.  
+                    byte[] msg = Encoding.ASCII.GetBytes("END\r\n");
+
+                    // Send the data through the socket.  
+                    int bytesSent = sender.Send(msg);
+
+                    // Receive the response from the remote device.  
+                    int bytesRec = sender.Receive(bytes);
+                    //Console.WriteLine("Echoed test = {0}",
+                    //    Encoding.ASCII.GetString(bytes, 0, bytesRec));
+
+                    // Release the socket.  
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+
+                }
+                catch (ArgumentNullException )
+                {
+                    //Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+                }
+                catch (SocketException )
+                {
+                    //Console.WriteLine("SocketException : {0}", se.ToString());
+                }
+                catch (Exception )
+                {
+                    //Console.WriteLine("Unexpected exception : {0}", e.ToString());
+                }
+
+            }
+            catch (Exception )
+            {
+                //Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
